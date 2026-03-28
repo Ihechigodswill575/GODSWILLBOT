eb666f4f3a210e2276eb376feb9788702c6f4f46
Ihechigodswill575 <Ihechigodswill575@gmail.com>
2026-03-28 23:07:48 +0100

    Add files via upload

---
+++ reactions.js
@@ -0,0 +1,8 @@
+module.exports = {
+    cry: '😢', hug: '🤗', slap: '👋', pat: '🫶',
+    wink: '😉', dance: '💃', bonk: '🔨', bite: '😬',
+    cuddle: '🥰', blush: '😊', wave: '👋', smile: '😊',
+    laugh: '😂', angry: '😤', love: '❤️', cool: '😎',
+    nom: '😋', yeet: '🚀', bully: '😈', kill: '💀',
+    poke: '👉', lick: '👅', highfive: '🙌',
+}
