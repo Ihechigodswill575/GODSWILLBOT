b69e4f23f79cf09206fc7aa6b5fd4263d655213a
Ihechigodswill575 <Ihechigodswill575@gmail.com>
2026-03-28 23:04:27 +0100

    Add configuration file for bot settings

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    OWNER_NUMBER: '2348145688688',
+    OWNER_NAME: 'TAVIK(GODSWILL)',
+    BOT_NAME: 'TAVIK BOT',
+    BOT_VERSION: 'V1.0',
+    UNSPLASH_KEY: 'lE3LTM9IWIahm1jhcw6_Gn8L2_6hnyzK-NBrg6urD5w',
+    PREFIX: '.',
+    BAD_WORDS: ['fuck', 'shit', 'bitch', 'bastard', 'idiot', 'stupid'],
+}
