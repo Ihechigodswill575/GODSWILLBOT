6b873f6b9de4354e303f034cbaed8bc24086c37e
Ihechigodswill575 <Ihechigodswill575@gmail.com>
2026-03-28 23:06:35 +0100

    Add files via upload

---
+++ state.js
@@ -0,0 +1,14 @@
+// Global state for TAVIK BOT
+module.exports = {
+    sudoUsers: [],
+    floodActive: {},
+    antiDelete: {},
+    autoreply: {},
+    antibadword: {},
+    antilink: {},
+    antispam: {},
+    autoread: false,
+    autoreact: false,
+    autotyping: false,
+    spamCount: {},
+}
